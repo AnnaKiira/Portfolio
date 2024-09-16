a2c7044a02ef6e0b6e72dc2379d97a3b61e7a5b5
User Name <annakirath@gmail.com>
2024-09-16 15:55:50 +0100

    finished my skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,36 +2,114 @@ import React from 'react'
 import './Skills.css'
 
 function Skills() {
-  return (
-    <section id='skills'>
-        About section
+    return (
+        <section id='skills'>
+            <span className='skillsTitle'>My skills</span>
+            <span className='skillsDesc'>I have experience working with the languages and frameworks in this section etc. etc. etc.</span>
+            <div className='skillIcons'>
+                <div className='skillIcon'>
+                <i class="devicon-javascript-plain"></i>
+                <div className='skillText'>
+                    <h2>JavaScript</h2>
+                </div>
+                </div>
 
-        <i class="devicon-javascript-plain"></i>
-        <i class="devicon-html5-plain"></i>
-        <i class="devicon-css3-plain"></i>
-        <i class="devicon-react-original"></i>
-        <i class="devicon-github-original"></i>
-        <i class="devicon-postman-plain"></i>
-        <i class="devicon-nodejs-plain-wordmark"></i>
-        <i class="devicon-npm-original-wordmark"></i>
-        <i class="devicon-express-original"></i>
-        <i class="devicon-python-plain"></i>
-        <i class="devicon-django-plain"></i>
-        <i class="devicon-postgresql-plain"></i>
-        <i class="devicon-mongodb-plain-wordmark"></i>
-        <i class="devicon-mongoose-original"></i>
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-    </section>
-  )
+                <div className='skillIcon'>
+                <i class="devicon-html5-plain"></i>
+                <div className='skillText'>
+                    <h2>HTML</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-css3-plain"></i>
+                <div className='skillText'>
+                    <h2>CSS</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-react-original"></i>
+                <div className='skillText'>
+                    <h2>React.js</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-github-original"></i>
+                <div className='skillText'>
+                    <h2>Github</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-postman-plain"></i>
+                <div className='skillText'>
+                    <h2>Postman</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-nodejs-plain-wordmark"></i>
+                <div className='skillText'>
+                    <h2>Node.js</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-npm-original-wordmark"></i>
+                <div className='skillText'>
+                    <h2>npm</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-express-original"></i>
+                <div className='skillText'>
+                    <h2>Express.js</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-python-plain"></i>
+                <div className='skillText'>
+                    <h2>Python</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-django-plain"></i>
+                <div className='skillText'>
+                    <h2>Django</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-postgresql-plain"></i>
+                <div className='skillText'>
+                    <h2>PostgreSQL</h2>
+                </div>
+                </div>
+
+                <div className='skillIcon'>
+                <i class="devicon-mongodb-plain-wordmark"></i>
+                <div className='skillText'>
+                    <h2>MongoDB</h2>
+                </div>
+                </div>
+
+
+                <div className='skillIcon'>
+                <i class="devicon-mongoose-original"></i>
+                <div className='skillText'>
+                    <h2>Mongoose</h2>
+                </div>
+                </div>
+            </div>
+
+
+        </section>
+    )
 }
 
 export default Skills
